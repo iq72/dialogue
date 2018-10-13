f6e42369e71e8e8679f60c6d6bdde1957a6a2d9c
leo <liukyzy008@gmail.com>
2018-10-13 22:41:03 +0800

    readabilityInproved

--- src/Components/Block.js
+++ src/Components/Block.js
@@ -6,8 +6,10 @@ const Block = styled.section`
     max-width:960px;
     flex-direction:row;
     align-items:flex-start;
-    margin:${props=>props.mode==='edit'?'0 ':'0.5rem 1rem'};
+    margin:${props=>props.mode==='edit'?'0 ':'1rem 1rem 0.5rem 1rem'};
     clear:both;
+    ${props=>props.mode==='add'&&"border-left : solid 4px"};
+    border-left-color:${props=>props.actor==='shopkeeper'?'#09c6da':'#f582e1'};
 `
 
 export default Block
