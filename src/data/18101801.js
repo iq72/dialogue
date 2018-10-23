4ca2f10a78422a05555f79c958ffc0d817e9fe7a
iq72 <liukyzy008@gmail.com>
2018-10-23 10:29:02 +0800

    addTestData

---
+++ src/data/18101801.js
@@ -0,0 +1,410 @@
+export const data={
+  inputBox: {
+    type: 'talk',
+    actor: 'shopkeeper',
+    mode: 'edit',
+    text: '整机一年，屏幕、主板三年'
+  },
+  dialogues: [
+    {
+      actor: 'customer',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '我要一个39寸的电视，有多宽'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '我们这有个图，您看一下呢。'
+        },
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '这个电视是50寸的110厘米，39寸的88，快90厘米。'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '底座是什么样子的呢'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          type: 'act',
+          mode: 'text',
+          text: '指旁边的60Q5T'
+        },
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '底座就是这样的，两边漏出来一点，大概2-3公分吧'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '没有那种在中间的小底座？'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '现在都是这种两边的了'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '指着50Q5T',
+          type: 'act'
+        },
+        {
+          mode: 'text',
+          text: '妻：这个有什么功能',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          mode: 'text',
+          text: '这个是CHiQ智能电视，按欧盟标准……',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '妻：智能不需要',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '夫：43寸的多大，我看一下。我们的桌子是100的，要在上面摆得下的，尽量大。'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          type: 'act',
+          mode: 'text',
+          text: '指着尺寸图'
+        },
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '43寸的96厘米'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '加上底座呢？'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '。。。'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '妻：算了，你的桌子只有1米，摆在上面满满当当的，容易掉，头重脚轻也不好看。就39寸的就可以了'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '。。。'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '43寸的是多宽？'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          type: 'talk',
+          mode: 'text',
+          text: '88公分，加上底座的话，90多点吧'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '嗯，1米减90，两边还剩一点，行，就这个吧',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '妻：这个是“如懿传”吧',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          mode: 'text',
+          text: '是的',
+          type: 'talk'
+        },
+        {
+          mode: 'text',
+          text: '语音「我要看如懿传」，50Q5T开始播放如懿传',
+          type: 'act'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '直接说话就可以了是吧，她们是跟我说这个挺好的',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '妻：这个好不好看，她们都给我推荐这个，跟甄嬛比好不好看',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          mode: 'text',
+          text: '我也断断续续看的，还不错，不过还是比甄嬛差一点',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '夫：这个多少钱',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          mode: 'text',
+          text: '2298，我们26号有个活动可以便宜200，现在也可以按这个价格给您，但是发票要到时候在给你，您要不要发票呢',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '推了一下老婆',
+          type: 'act'
+        },
+        {
+          mode: 'text',
+          text: '你说呢',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '妻：啊，我在看电视了',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          mode: 'text',
+          text: '现在就要的话，就要多200块钱',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '行嘛，多200就多200吧',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          mode: 'text',
+          text: '。。。',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          mode: 'text',
+          text: '只是发票要晚一点，26号之后再给您，机器还是现在就可以开出来的',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '没事，就开单吧',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '妻：就发票迟点给吧，能便宜一点干嘛不便宜；那是2099是吧',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          mode: 'text',
+          text: '是的，您在这里留一下姓名地址，方便送货',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'customer',
+      contents: [
+        {
+          mode: 'text',
+          text: '你们这个保修怎么算',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          mode: 'edit',
+          text: '整机一年，屏幕、主板三年',
+          type: 'talk'
+        }
+      ]
+    },
+    {
+      actor: 'shopkeeper',
+      contents: [
+        {
+          mode: 'text',
+          text: '',
+          type: 'talk'
+        }
+      ]
+    }
+  ]
+}
